31528591fbb948a642438ddfd2f6232851c64849
MacroUniverse <270174408@qq.com>
2018-11-02 17:04:48 -0500

    update

--- Physics/Optics/gaussian_pulse.m
+++ Physics/Optics/gaussian_pulse.m
@@ -1,7 +1,6 @@
 % generate a gaussian pulse $f(t)$ (complex)
-% sigma is full widths at half intensity
-% all in atomic units
+% sigma is full widths at half intensity (E0^2)
 
-function f = gaussian_pulse(E0, w0, sigma, tc, phi0)
-f = @(t) E0*exp(-1i*w0*(t-tc) - 2*log(2)*((t-tc)/sigma).^2 + phi0);
+function f = gaussian_pulse(E0, w0, FWHM, tc, phi0)
+f = @(t) E0*exp(-1i*w0*(t-tc) - 2*log(2)*((t-tc)/FWHM).^2 + phi0);
 end
