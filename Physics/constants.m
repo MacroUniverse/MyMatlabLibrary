27cb7a0fdf49991195067390d0eac12a929f243a
MacroUniverse <270174408@qq.com>
2018-11-09 17:03:29 -0600

    update constants

--- Physics/constants.m
+++ Physics/constants.m
@@ -1,4 +1,8 @@
 % generate constants in physics and mathmetics
+% run to update "constants.mat"
+
+clear;
+cd(mfilepath);
 
 c0=299792458;                        % speed of light  
 mu0=4*pi*1e-7;                       % vacuum permeability 1.256637061435917e-06
@@ -20,20 +24,29 @@ a0=5.2917721067e-11;                 % Bohr radius
 % Mathematical Constants
 E=exp(1);                            % base of the natural logarithm
 
-% unit conversion
+% conversion constants
 
 inch=2.54e-2; % length
 gauss=1e-4;   % electric field
 mile=6.09e+2; % length
 lb=0.454; pound=0.454; % mass
-
-% atomic units conversion
-
 au_x = 5.2917721067e-11;  % 1 a.u. distance
 au_t = 2.418884326e-17;   % 1 a.u. time
 au_E = 4.3597446499e-18;  % 1 a.u. energy
 au_Ef = 5.1422067070e11;  % 1 a.u. electric field
 
+% conversion functions
+au_E2eV = @(x) 27.211386018871558*x; % au_E/e
+eV2au_E = @(x) 0.036749322482379*x; % e/au_E
+% wavelengt [nm] and photon energy [eV]
+nm2eV = @(x) 1239.841973964072/x; % 2*pi*c0*h1*1e9/e
+eV2nm = @(x) 1239.841973964072/x;
+% wavelengt [nm] and photon energy [au]
+nm2au_E = @(x) 45.56335252839456/x; % 2*pi*c0*h1*1e9/au_E
+au_E2nm = @(x) 45.56335252839456/x;
 % intensity (W/cm^2) TO electric field (a.u.)
-w_cm2_TO_au_Ef = @(x)5.338026811839179e-09*sqrt(x);
-au_Ef_TO_w_cm2 = @(x) 3.509445467042176e+16*x.^2;
+w_cm22au_Ef = @(x)5.338026811839179e-09*sqrt(x); % sqrt(2e4/epsilon0/c0/au_Ef^2)
+au_Ef2w_cm2 = @(x) 3.509445467042176e+16*x.^2; % epsilon0*c0*au_Ef^2*5e-5
+
+
+save constants.mat
