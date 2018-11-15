ce1b740741c5b6975723dff6fe20c66bab995347
MacroUniverse <270174408@qq.com>
2018-11-14 18:58:11 -0600

    Update System/mfilepath.m

--- System/mfilepath.m
+++ System/mfilepath.m
@@ -3,11 +3,29 @@
 % 'name' does not include '.m'
 
 function [path, name] = mfilepath
+% try to use call stack, file don't have to be opened
 s = dbstack(1,'-completenames');
-path = s(1).file;
-name = s(1).name;
-if isequal(name, 'LiveEditorEvaluationHelperESectionEval')
-    path = './'; name = ''; return
+if ~isempty(s)
+    path = s(1).file;
+    [~, name] = fileparts(path);
+    if ~isequal(name, 'LiveEditorEvaluationHelperESectionEval')
+        path = strrep(path, '\', '/');
+        path = [path '/'];
+        return;
+    end
 end
-path = strrep(path, ['/', name, '.m'], '/');
+
+% this function called from command line (or with F9 key)
+% get the current opened file
+% type 'doc matlab.desktop.editor' for documentation
+temp = matlab.desktop.editor.getActive();
+if isempty(temp)
+    warning('This function called in command line while no file is opened.');
+    path = './'; name = '';
+    return;
+end
+path = temp.Filename;
+[path, name] = fileparts(path);
+path = strrep(path, '\', '/');
+path = [path '/'];
 end
