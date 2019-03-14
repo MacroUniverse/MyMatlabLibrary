6cb313b23914a0faa3f1bf919de28d0f399f78f7
MacroUniverse <270174408@qq.com>
2019-03-14 01:15:11 -0500

    matt related update

--- Others/mattload.m
+++ Others/mattload.m
@@ -1,17 +1,19 @@
 % read ".matt" data file
 % consider using A = fscanf(fileID,formatSpec,sizeA) to read data in
 % different class types. Thus saving space.
+% varnames is a cell array containing the names of the variables exported
 
-function mattread(fname)
+function varargout = mattload(fname)
 %fileID = fopen(fname,'r');
 %if fileID == -1, error('file does not exist'); end
 %data = fscanf(fileID, '%f');
-data = dlmread(fname);
+data = dlmread(fname); data = data(:).';
 ind = 1;
 nvar = data(end);
+names = cell(1,nvar);
 for ii = 1:nvar
     namesize = data(ind); ind = ind + 1;
-    varname = char(data(ind : ind+namesize-1))';
+    names{ii} = char(data(ind : ind+namesize-1));
     ind = ind + namesize;
     matclass = data(ind); ind = ind + 1;
     ndim = data(ind); ind = ind + 1;
@@ -21,7 +23,7 @@ for ii = 1:nvar
         matsize = [1,data(ind)];
         ind = ind + 1;
     else % ndim > 1
-        matsize = data(ind : ind+ndim-1)';
+        matsize = data(ind : ind+ndim-1);
         ind = ind + ndim;
     end
     if matclass >= 20 && matclass < 60 % floating point types
@@ -34,7 +36,11 @@ for ii = 1:nvar
     matnumel = prod(matsize);
     A(:) = data(ind:ind+matnumel-1);
     ind = ind + matnumel;
-    assignin('caller',varname,A);
+    assignin('caller',names{ii},A);
 end
-%fclose(fileID);
+if nargout == 0
+    varargout = {};
+else
+    varargout{1} = names;
+end
 end
