785300fcc933e8e7aee84c262349e0817065d17b
MacroUniverse <270174408@qq.com>
2019-04-09 12:52:44 -0500

    fix bugs

--- Math/Math Functions/fzeroN.m
+++ Math/Math Functions/fzeroN.m
@@ -2,18 +2,29 @@
 %fun is the function to be found 0
 %interval is the total interval to find roots
 %resolution is the number of intervals to use bisection
+% if abs(fun(root)) < CvgTol, it's considered a converge root
 
-function roots = fzeroN(fun,interval,resolution)
+function roots = fzeroN(fun,interval,resolution,CvgTol)
+if ~exist('CvgTol','var') || isempty(CvgTol)
+    CvgTol = inf; 
+end
 
 x=linspace(interval(1),interval(end), resolution);
 y=arrayfun(fun,x);
-figure; plot(x,y,'+-')
+figure; plot(x,y,'.-'); hold on;
 title('the function to be found zeros of');
 Sign=sign(y);
 ind=find(Sign(1:end-1).*Sign(2:end)<0);
 
-roots=zeros(size(ind));
+roots=zeros(size(ind)); kk = 0;
 for ii=1:numel(ind)
-    roots(ii)=fzero(fun,[x(ind(ii)),x(ind(ii)+1)]);  
+    plot([x(ind(ii)),x(ind(ii)+1)], [fun(x(ind(ii))),fun(x(ind(ii)+1))], '.-r');
+    [root, val] = fzero(fun,[x(ind(ii)),x(ind(ii)+1)]);
+    if abs(val) < CvgTol
+        kk = kk + 1;
+        roots(kk) = root;
+        scatter(root, fun(root));
+    end
 end
+roots = roots(1:kk);
 end
--- Physics/Quantum_Mechanics/TISE/bound_shooting.m
+++ Physics/Quantum_Mechanics/TISE/bound_shooting.m
@@ -9,67 +9,16 @@ function [Eng, X, Psi] = bound_shooting(V, xmin, xmid, xmax, mass, ...
 
 % find the zeros in Psi(x_max) vs E plot
 trial_fun = @(E) bound_shooting_trial(E, V, xmin, xmid, xmax, mass, odeOpt);
-Eng = fzeroN(trial_fun, Espan, EResolution);
+Eng = fzeroN(trial_fun, Espan, EResolution,0.01);
 
 % output and plot.
 NE = numel(Eng);
 X = cell(1,NE); Psi = X;
 for ii = 1:numel(Eng)
-    [~, X{ii}, Psi{ii}] = trial_fun(Eng(ii));
-end
-end
-
-% output matching error determined by Wronskian
-% [psi_left, psi_left', psi_right; psi_right']
-% at xmid, psi normalized to max(psi) = 1
-function [err, x, psi] = bound_shooting_trial(E, V, xmin, xmid, xmax, mass, odeOpt)
-dy = 1e-16; % dy is psi(xmin) and psi(xmax)
-[x1, Y1] = bound_shooting_half(E, V, [xmin,xmid], mass, dy, odeOpt);
-[x2, Y2] = bound_shooting_half(E, V, [xmax,xmid], mass, dy, odeOpt);
-
-% error
-err = fmatch_err(Y1(end,1),Y1(end,2),Y2(end,1),Y2(end,2));
-
-psi1_max = max(abs(Y1(:,1)));
-psi2_max = max(abs(Y2(:,1)));
-
-% wave function (max(psi) = 1)
-if nargout > 1
-    x = [x1; flip(x2(1:end-1))].';
-end
-if nargout > 2
-    v1 = Y1(end,:);
-    v2 = Y2(end,:);
-    scale = sign(dot(v1,v2))*norm(v1)/norm(v2);
-    if psi1_max >= psi2_max * scale
-        psi = [
-            Y1(:,1).' / psi1_max,  .....
-            flip(Y2(1:end-1,1)).' * (scale / psi1_max)];
-    else
-        psi = [
-            Y1(:,1).' / (scale * psi2_max),  .....
-            flip(Y2(1:end-1,1)).' / psi2_max];
+    [err, X{ii}, Psi{ii}] = trial_fun(Eng(ii));
+    if (err > 1e-3)
+        disp('bound state: '); disp(ii); disp('err ='); disp(err);
+        error('something went wrong!');
     end
 end
-end
-
-% solve TISE by shooting from one end of the potential to a middle point
-% the middle point xspan(2) should be in classically allowed region
-% varargin are options for ode45 solver
-% dy is psi(xmin) and psi(xmax)
-function [x, Y] = bound_shooting_half(E, V, xspan, mass, dy, odeOpt)
-if V(xspan(1)) > E
-    Y0 = [1; sqrt(2*mass*(V(xspan(1)) - E))] * dy;
-else
-    Y0 = [0; 1];
-end
-odefun = @(x,Y) TISE_odefun(x,Y,E,V,mass);
-[x,Y] = ode45(odefun, xspan, Y0, odeOpt);
-end
-
-% ode function
-% if Y = [psi(x); psi'(x)] is known, find dY(x) = [psi'(x), psi''(x)];
-function dY =TISE_odefun(x,Y,E,V,mass)
-dY(2,1) = - 2*mass*(E-V(x))*Y(1);
-dY(1) = Y(2);
 end
