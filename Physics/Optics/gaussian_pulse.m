b493d4d57f8140af40363937638cc7e0e515b798
MacroUniverse <270174408@qq.com>
2019-05-16 15:18:27 -0500

    Update gaussian_pulse.m

--- Physics/Optics/gaussian_pulse.m
+++ Physics/Optics/gaussian_pulse.m
@@ -2,5 +2,5 @@
 % sigma is full widths at half intensity (E0^2)
 
 function f = gaussian_pulse(E0, w0, FWHM, tc, phi0)
-f = @(t) E0*exp(-1i*w0*(t-tc) - 2*log(2)*((t-tc)/FWHM).^2 + phi0);
+f = @(t) E0*exp(-2*log(2)*((t-tc)/FWHM).^2 -1i*w0*(t-tc) + 1i*phi0);
 end
