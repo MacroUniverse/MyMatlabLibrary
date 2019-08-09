8419dee194b49cbafd35b449d598d2f942fc7061
MacroUniverse <270174408@qq.com>
2019-08-09 17:10:23 -0500

    Update im2gif.m

--- Image_Processing/im2gif.m
+++ Image_Processing/im2gif.m
@@ -1,18 +1,20 @@
 function im2gif()
 % === params =====
-fname = 'wv3bc.gif';
-Ncolor = 32;
-dt = 0.1;
+fname = 'JAD.gif';
+Ncolor = 256; % gif support a maximum of 256 (index must be uint8 type)
+dt = 0.4;
 % ================
 [filename,path] = uigetfile('*.*', 'multiselect', 'on');
 cd(path);
 I = imread(filename{1});
-[X,cmap] = rgb2ind(I,Ncolor);
+[X,cmap] = rgb2ind(I,Ncolor,'nodither');
 imwrite(X, cmap, fname, 'gif', 'Loopcount', inf, 'DelayTime', dt);
+figure;
 for ii = 2:numel(filename)
-I = imread(filename{ii});
-[X,cmap] = rgb2ind(I,Ncolor);
-imwrite(X, cmap, fname, 'gif', 'WriteMode', ....
-        'append', 'DelayTime', dt);
+    I = imread(filename{ii});
+    [X,cmap] = rgb2ind(I,Ncolor,'nodither');
+    imshow(X, cmap); drawnow;
+    imwrite(X, cmap, fname, 'gif', 'WriteMode', ....
+            'append', 'DelayTime', dt);
 end
 end
