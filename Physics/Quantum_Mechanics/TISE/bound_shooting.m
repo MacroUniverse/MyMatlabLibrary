8bc316ad3c5fa7694823f41a2b50c069fa9ad652
MacroUniverse <270174408@qq.com>
2019-09-15 02:08:03 -0500

    update bound_shooting

--- Physics/Quantum_Mechanics/TISE/bound_shooting.m
+++ Physics/Quantum_Mechanics/TISE/bound_shooting.m
@@ -5,7 +5,7 @@
 % psi(:,i) is the wave function of Eng(i)
 % output Y = [psi(x); psi'(x)]
 function [Eng, X, Psi] = bound_shooting(V, xmin, xmid, xmax, mass, ...
-    Espan, EResolution, odeOpt, plot_flat)
+    Espan, EResolution, odeOpt, plot_flag)
 
 % find the zeros in Psi(x_max) vs E plot
 trial_fun = @(E) bound_shooting_trial(E, V, xmin, xmid, xmax, mass, odeOpt);
@@ -13,23 +13,23 @@ Eng = fzeroN(trial_fun, Espan, EResolution);
 
 % output, check nodes
 X = cell(1,numel(Eng)); Psi = X;
-for ii = 1:numel(Eng)
-    [~, X{ii}, Psi{ii}] = trial_fun(Eng(ii));
-    if numzero(Psi{ii}) > ii - 1
-        warning(['missed bound state ', num2str(ii),...
+for i = 1:numel(Eng)
+    [~, X{i}, Psi{i}] = trial_fun(Eng(i));
+    if numzero(Psi{i}) > i - 1
+        warning(['missed bound state ', num2str(i),...
             ', increase Eresolution!']);
-    elseif numzero(Psi{ii}) < ii - 1
+    elseif numzero(Psi{i}) < i - 1
         error('duplicate bound state found!');
     end
     
-    if plot_flat
-        x = X{ii}; psi = Psi{ii};
+    if plot_flag
+        x = X{i}; psi = Psi{i};
         figure; plot(x,psi); axis([x(1),x(end),-1.2,1.2]);
         hold on;
         xlabel('x'); ylabel('\psi(x)');
         Nzeros = numzero(psi);
-        title(['i = ', num2str(ii),...
-            ', E_{', num2str(Nzeros+1), '} = ',  num2str(Eng(ii))]);
+        title(['i = ', num2str(i),...
+            ', E_{', num2str(Nzeros+1), '} = ',  num2str(Eng(i))]);
         scatter(xmid,0);
     end
 end
