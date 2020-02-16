2aeb436672986294265fb52ebab74de61f608ac3
MacroUniverse <270174408@qq.com>
2020-02-15 23:48:21 -0600

    Update fit_peak.m

--- Math/fitting/fit_peak.m
+++ Math/fitting/fit_peak.m
@@ -3,8 +3,12 @@
 function [xp, yp] = fit_peak(x, y, order)
 p = polyfit(x, y, order);
 dp = polyder(p);
- xp = fzero(@(x)polyval(dp, x), [x(1), x(end)]);
- yp = polyval(p, xp);
- % figure; plot(x, y); hold on;
- % plot(x, polyval(p, x)); scatter(xp, yp);
+f = @(x)polyval(dp, x);
+if (f(x(1)) * f(x(end)) >= 0)
+    figure; plot(x, y); hold on;
+end
+xp = fzero(f, [x(1), x(end)]);
+yp = polyval(p, xp);
+% figure; plot(x, y); hold on;
+% plot(x, polyval(p, x)); scatter(xp, yp);
 end
