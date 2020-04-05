61353da5b737ba82e7bda9f870680ae4ae321be3
MacroUniverse <270174408@qq.com>
2020-04-04 23:40:15 -0500

    update

--- Visualization/colormap/colormap_jet_demo.m
+++ Visualization/colormap/colormap_jet_demo.m
@@ -1,8 +1,9 @@
 % colormap_jet_demo
 N = 1000;
 val = linspace(0, 1, N);
-rgb = colormap_jet(val);
+% rgb = colormap_jet(val);
 % rgb = colormap_jet2(val);
+rgb = colormap_spec(val);
 rgb1 = uint8(zeros(1, N, 3));
 rgb1(1,:,:) = rgb;
 rgb2 = repmat(rgb1, round(N/6), 1);
--- Visualization/colormap/colormap_spec.m
+++ Visualization/colormap/colormap_spec.m
@@ -1,6 +1,7 @@
 % generate a model spectrum
 % val must be from 0 to 1, corresponding to 400nm to 800nm
 function rgb = colormap_spec(val)
-lambda = 430 + (650-430)*val;
+lamb_min = 425; lamb_max = 650;
+lambda = lamb_min + (lamb_max-lamb_min)*val;
 rgb = nm2rgb(lambda);
 end
