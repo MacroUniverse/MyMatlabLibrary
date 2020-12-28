5d2d5a7b70d3e6b42c0b103865656edef73d3da4
MacroUniverse <270174408@qq.com>
2020-12-28 01:22:55 -0600

    Update surfSph.m

--- Visualization/surfSph.m
+++ Visualization/surfSph.m
@@ -1,16 +1,33 @@
 % surf() in spherical coordinate
-% R, Th, Ph are 2D matrices
-% 
-
-function h = surfSph(R,Th,Ph,varargin)
-if isvector(R) == 1 && isvector(Th) == 1
+% usage 1. R,Th,Ph,Val are 2D matrices in same shape
+% usage 2. R,Th,Ph are vector,vector,scalar
+% usage 3. R,Th,Ph are vector,scalar,vector
+% surf() in spherical coordinate
+% usage 1. R,Th,Ph,Val are 2D matrices in same shape
+% usage 2. R,Th,Ph are vector,vector,scalar
+% usage 3. R,Th,Ph are vector,scalar,vector
+function h = surfSph(R,Th,Ph,Val,varargin)
+% resize
+if isvector(R) && isvector(Th)
     [R,Th] = ndgrid(R, Th);
 end
-if Ph == 1
+if isvector(R) && isvector(Ph)
+    [R,Ph] = ndgrid(R,Ph);
+end
+if isscalar(Th)
+    Th = Th*ones(size(R));
+end
+if isscalar(Ph)
     Ph = Ph*ones(size(R));
 end
+if ~isequal(size(R),size(Th)) || ...
+    ~isequal(size(Th),size(Ph)) || ...
+    ~isequal(size(Ph),size(Val))
+    error('wrong shape for Th, Ph, Val');
+end
+% plot
 [X,Y,Z] = sph2cart(Ph,pi/2-Th,R);
-h = surf(X,Y,Z,varargin{:});
+h = surf(X,Y,Z,Val,varargin{:});
 shading flat; axis equal;
 xlabel x; ylabel y; zlabel z;
 set(datacursormode(gcf), 'UpdateFcn', @datatip);
