2fb3377c33e2b4a02937b556d62c45708d2c388d
MacroUniverse <270174408@qq.com>
2021-01-15 21:34:04 -0600

    au_t & fs conversion

---
+++ Physics/au_t2fs.m
@@ -0,0 +1,4 @@
+% convert atomic unit time to femto-second (1e-15s)
+function fs = au_t2fs(au_t)
+fs = au_t * 24.18884326576744e-3;
+end
---
+++ Physics/fs2au_t.m
@@ -0,0 +1,4 @@
+% convert femto-second (1e-15s) to atomic unit time
+function au_t = fs2au_t(as)
+au_t = as / 24.18884326576744e-3;
+end
