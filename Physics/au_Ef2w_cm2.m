c71fa05f4a0d094c2df204207884af06f5e40e72
MacroUniverse <270174408@qq.com>
2021-02-06 15:58:29 -0600

    comments

--- Physics/au_Ef2w_cm2.m
+++ Physics/au_Ef2w_cm2.m
@@ -1,4 +1,5 @@
 % convert electric field (au) to laser intensity (w/cm^2)
+% verified, Brandsen eq 15.47
 function w_cm2 = au_Ef2w_cm2(au)
 load constants.mat epsilon0 c0 au_Ef
 w_cm2 = 0.5e-4*epsilon0*c0*(au_Ef.*au).^2;
--- Physics/w_cm22au_Ef.m
+++ Physics/w_cm22au_Ef.m
@@ -1,4 +1,5 @@
 % convert laser intensity (w/cm^2) to electric field (au)
+% verified, Brandsen eq 15.47
 function au = w_cm22au_Ef(w_cm2)
 load constants.mat epsilon0 c0 au_Ef
 au = sqrt(w_cm2./(0.5e-4*epsilon0*c0))./au_Ef;
