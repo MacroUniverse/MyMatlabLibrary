6184b7f8e993f9c4e0f9daf4083bf918f2264593
MacroUniverse <270174408@qq.com>
2021-07-21 12:36:08 -0500

    amend

--- Math/Fourier Transform/FFT_demo.m
+++ Math/Fourier Transform/FFT_demo.m
@@ -1,7 +1,7 @@
 % see FTExp_ex2
-clear;
-a = 1/4; k0 = 4;
-x_shift = pi/(2*a);
+clear; close all;
+a = 1/4; k0 = 6;
+x_shift = 10;
 Nx = 5000;
 x = linspace(-pi/(2*a), pi/(2*a), Nx) + x_shift;
 dx = (x(end)-x(1))/(Nx-1);
@@ -19,8 +19,12 @@ g = sqrt(2*pi)*a./(4*a^2-(k-k0).^2).*sinc((pi*(k-k0))/(2*a));
 g = g .* exp(-1i*k*x_shift);
 
 figure;
-plot(k, g, '.-'); hold on;
-plot(k1, g1, 'o');
-axis([1, 7, -1.5, 2.5]);
-plot(k2, g2, '.');
+plot(k, real(g), '.-'); hold on;
+plot(k1, real(g1), 'o');
+plot(k2, real(g2), '.');
+axis([k0-3, k0+3, -2.7, 2.7]);
 legend('analytical', 'FFT', 'FFT interp');
+
+disp(['norm2(f) = ' num2str(sum(abs(f).^2)*dx, 14)]);
+disp(['norm2(g) = ' num2str(sum(abs(g).^2)*(k(2)-k(1)), 14)]);
+disp(['norm2(g1) = ' num2str(sum(abs(g1).^2)*(k1(2)-k1(1)), 14)]);
