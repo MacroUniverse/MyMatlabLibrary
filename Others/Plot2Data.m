247239864de68f95054b72f96657563a991c9ce8
MacroUniverse <270174408@qq.com>
2021-12-17 16:13:15 -0600

    Update Plot2Data.m

--- Others/Plot2Data.m
+++ Others/Plot2Data.m
@@ -1,40 +1,39 @@
 % Data Extractor for Plot
 % x and y are both scaled to the range [0,1]
-function [x,y]=Plot2Data
-if nargout~=2
-    error('Must use 2 output variables, or data will be lost');
-end
-disp('select image file')
+function [x,y] = plot2data
+disp('请选择图片')
 [filename,path]=uigetfile('*.*');
 cd(path);
 I = imread(filename);
 figure; imshow(I); hold on;
-disp('zoom in to select origin (7second)');
+disp('放大图片以选取坐标原点 (7s)');
 pause(7);
-disp('select origin')
+disp('选取坐标原点')
 P0 = ginput(1); scatter(P0(1),P0(2));
 
-disp('zoom in to select x maximum (7sec)');
+disp('放大图片以选取 x 轴最大值 (7s)');
 pause(7);
-disp('select x maximum')
+disp('选取 x 最大值')
 Px = ginput(1); scatter(Px(1),Px(2));
 
-disp('zoom in to select y maximum (7sec)');
+disp('放大图片以选取 y 轴最大值 (7sec)');
 pause(7);
-disp('select y maximum')
+disp('选取 y 最大值')
 Py = ginput(1); scatter(Py(1),Py(2));
 P = [];
 while 1
-    disp('zoom in to select points (7sec)');
+    disp('放大图片以选取图中一组数据点 (7sec)');
     pause(7);
-    disp('select points')
+    disp('选取图中一组数据点')
     temp = ginput;
     if ~isempty(temp)
         scatter(temp(:,1),temp(:,2));
         P = [P; temp];
-        save Plot2Data.mat
+        if nargout == 0
+            save plot2data.mat
+        end
     end
-    temp = input('continue:[],end:1');
+    temp = input('继续:回车; 结束:1');
     if ~isempty(temp)
         break;
     end
