3ac1bc1a3661fc0d4e4b6831cdbe4cc3d451d733
MacroUniverse <270174408@qq.com>
2022-05-07 02:18:01 -0500

    Update D_i.m

--- Math/Math Functions/D_i.m
+++ Math/Math Functions/D_i.m
@@ -1,8 +1,8 @@
 % 数值偏微分
 function ret = D_i(f, i, x, h)
-x(i) = x(i) - h;
+x(i) = x(i) - 0.5*h;
 f1 = f(x);
-x(i) = x(i) + 2*h;
+x(i) = x(i) + h;
 f2 = f(x);
-ret = (f2 - f1)/(2*h);
+ret = (f2 - f1)/h;
 end
