490cf392c978c82fba6a7e0f41cc63b00999eaee
MacroUniverse <270174408@qq.com>
2022-06-26 02:36:34 -0500

    Update matbload.m

--- Others/matbload.m
+++ Others/matbload.m
@@ -2,16 +2,22 @@
 % varnames is a cell array containing the names of the variables exported
 % use mattload(fname, 'var1', 'var2', ...) to load specific variables
 
-function names = matbload(fname, varargin)
-Nvar = 0;
-names = cell(1,Nvar);
+function matbload(fname, varargin)
 fid = fopen(fname, 'r');
-
-for ii = 1:Nvar
+for ii = 1:1e10
     Nname = fread(fid, 1, 'int64');
-    names{ii} = fread(fid, [1,Nname], '*char');
+    if (Nname > 100)
+        break;
+    end
+    varname = fread(fid, [1,Nname], '*char');
     matclass = fread(fid, 1, 'int64'); % 21:double, 41:complex
+    if ~(matclass >= 0 && matclass < 60)
+        break;
+    end
     ndim = fread(fid, 1, 'int64');
+    if ndim > 4
+        break;
+    end
     if (ndim == 0) % scalar
         matsize = [1,1];
     elseif (ndim == 1)
@@ -25,13 +31,19 @@ for ii = 1:Nvar
     else
         load_it = false;
         for jj = 1:numel(varargin)
-            if strcmp(names{ii}, varargin{jj})
+            if strcmp(varname, varargin{jj})
                 load_it = true; break;
             end
         end
     end
     if (load_it)
-        if matclass == 21 % double
+        if matclass == 1
+            data = fread(fid, matnumel, 'char');
+        elseif matclass == 2
+            data = fread(fid, matnumel, 'int32');
+        elseif matclass == 3
+            data = fread(fid, matnumel, 'int64');
+        elseif matclass == 21 % double
             data = fread(fid, matnumel, 'double');
         elseif matclass == 41 % complex
             data = fread(fid, matnumel*2, 'double');
@@ -39,8 +51,9 @@ for ii = 1:Nvar
         else
             error('not implemented!');
         end
+        if feof(fid), break; end
         data = reshape(data, matsize);
-        assignin('caller', names{ii}, data);
+        assignin('caller', varname, data);
     else
         if matclass == 21 % double
             seekg(fid, matnumel*8, 'cof');
