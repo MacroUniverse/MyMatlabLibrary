23bf40988e2396d131fb90fc62ad54c63a4f23ca
MacroUniverse <270174408@qq.com>
2022-09-22 12:24:52 -0500

    Update startup.m

--- System/startup.m
+++ System/startup.m
@@ -1,10 +1,14 @@
 % This script is automatically executed when Matlab starts
+% make sure this file is in Matlab search path
 % remember to 'clear' after each section
 
 %% use old figure toolbar if Matlab version >= 9.5 (R2018b)
 % the new one sucks!
 v = ver('MATLAB');
-if str2double(v.Version) > 9.45
+int_dot = strfind(v.Version, '.');
+vMajor = str2double(v.Version(1:int_dot-1));
+vMinor = str2double(v.Version(int_dot+1:end));
+if vMajor >= 9 && vMinor >= 5
     set(groot,'defaultFigureCreateFcn',....
         'addToolbarExplorationButtons(gcf)');
     set(groot,'defaultAxesCreateFcn',....
@@ -13,4 +17,3 @@ end
 clear;
 
 %% add more code below
-
