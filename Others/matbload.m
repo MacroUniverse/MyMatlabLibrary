57391cf5c12ea5f84e31d15f79d1515557081ce0
MacroUniverse <270174408@qq.com>
2023-02-26 00:42:56 -0600

    1

--- Others/matbload.m
+++ Others/matbload.m
@@ -57,7 +57,7 @@ for ii = 1:numvars
             data = fread(fid, matnumel, 'int32');
         elseif matclass == 3
             data = fread(fid, matnumel, 'int64');
-        elseif matclass == 21 % double
+        elseif matclass == 21
             data = fread(fid, matnumel, 'double');
         elseif matclass == 41 % complex
             data = fread(fid, matnumel*2, 'double');
@@ -72,7 +72,7 @@ for ii = 1:numvars
         if Nloaded == numel(varargin)
             break;
         end
-    else
+    else % skip this variable
         if matclass == 1 % char
             fseek(fid, matnumel, 'cof');
         elseif matclass == 2 % int32
